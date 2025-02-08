7284cdcd23c5331f4bfd0c5f4c6c5b2183539adf
pomi yanuar <tobir0665@gmail.com>
2025-02-09 03:22:46 +0700

    register authentikasi dan store data user

--- lib/Login/resgister_screen.dart
+++ lib/Login/resgister_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ResgisterScreen extends StatefulWidget {
@@ -8,21 +11,89 @@ class ResgisterScreen extends StatefulWidget {
 }
 
 class _ResgisterScreenState extends State<ResgisterScreen> {
-  bool isLoading = false;
+  bool isFirebaseInitialized = false;
+
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  String _errorMessage = '';
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> _register() async {
+    String username = _usernameController.text.trim();
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    // Validasi input
+    if (username.isEmpty || email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Semua kolom harus diisi")),
+      );
+      return;
+    }
+
+    try {
+      // Mendaftar akun menggunakan Firebase Authentication
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Menyimpan data username di Firestore
+      await _firestore.collection('users').doc(userCredential.user?.uid).set({
+        'username': username,
+        'email': email,
+        'createdAt': Timestamp.now(),
+      });
+
+      // Berhasil mendaftar
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content:
+                Text("Akun berhasil dibuat: ${userCredential.user?.email}")),
+      );
+    } on FirebaseAuthException catch (e) {
+      // Menangani error saat pendaftaran
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Pendaftaran gagal: ${e.message}")),
+      );
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    _initializeFirebase();
+  }
+
+  Future<void> _initializeFirebase() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        isFirebaseInitialized = true;
+      });
+    } catch (e) {
+      print("Error initializing Firebase: $e");
+      setState(() {
+        isFirebaseInitialized = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       resizeToAvoidBottomInset:
           true, // Menyesuaikan dengan ukuran layar ketika keyboard muncul
       body: SingleChildScrollView(
-        child: isLoading
+        child: _isLoading
             ? Container(
                 child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -59,6 +130,8 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         SizedBox(height: 16.0),
+
+                        // Username Field
                         Text(
                           "Username",
                           textAlign: TextAlign.left,
@@ -69,6 +142,7 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                         ),
                         SizedBox(height: 3.0),
                         TextFormField(
+                          controller: _usernameController,
                           decoration: InputDecoration(
                             filled: true,
                             prefixIcon: Padding(
@@ -82,8 +156,16 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                             contentPadding: EdgeInsets.symmetric(
                                 vertical: 12.0, horizontal: 8.0),
                           ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Username tidak boleh kosong';
+                            }
+                            return null;
+                          },
                         ),
                         SizedBox(height: 8.0),
+
+                        // Email Field
                         Text(
                           "Email",
                           textAlign: TextAlign.left,
@@ -94,6 +176,7 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                         ),
                         SizedBox(height: 3.0),
                         TextFormField(
+                          controller: _emailController,
                           decoration: InputDecoration(
                             filled: true,
                             prefixIcon: Padding(
@@ -107,9 +190,20 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                             contentPadding: EdgeInsets.symmetric(
                                 vertical: 12.0, horizontal: 8.0),
                           ),
-                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email tidak boleh kosong';
+                            }
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Email tidak valid';
+                            }
+                            return null;
+                          },
                         ),
                         SizedBox(height: 8.0),
+
+                        // Password Field
                         Text(
                           "Password",
                           textAlign: TextAlign.left,
@@ -120,6 +214,7 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                         ),
                         SizedBox(height: 3.0),
                         TextFormField(
+                          controller: _passwordController,
                           decoration: InputDecoration(
                             filled: true,
                             prefixIcon: Padding(
@@ -135,12 +230,29 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                             suffixIcon: Icon(Icons.visibility),
                           ),
                           obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Password tidak boleh kosong';
+                            }
+                            if (value.length < 6) {
+                              return 'Password harus lebih dari 6 karakter';
+                            }
+                            return null;
+                          },
                         ),
                         SizedBox(height: 16.0),
+
+                        // Error Message
+                        if (_errorMessage.isNotEmpty)
+                          Text(
+                            _errorMessage,
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        SizedBox(height: 16.0),
+
+                        // Submit Button
                         ElevatedButton(
-                          onPressed: () {
-                            // Tindakan saat tombol ditekan
-                          },
+                          onPressed: _register,
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
                             shape: RoundedRectangleBorder(
@@ -150,19 +262,23 @@ class _ResgisterScreenState extends State<ResgisterScreen> {
                             padding: EdgeInsets.symmetric(
                                 vertical: 16.0, horizontal: 24.0),
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 10),
-                              Text(
-                                'Register',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
+                          child: _isLoading
+                              ? CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(width: 10),
+                                    Text(
+                                      'Register',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                            ],
-                          ),
                         ),
                       ],
                     ),
