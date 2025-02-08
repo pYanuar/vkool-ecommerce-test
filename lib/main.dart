c733ce2b7ec9bd147a3b91d76d86faa44c909c6c
pomi yanuar <tobir0665@gmail.com>
2025-02-09 01:39:43 +0700

    perubahan root di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vkool_ecommerce_test/splash_screen.dart';
+import 'package:vkool_ecommerce_test/Ui/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
